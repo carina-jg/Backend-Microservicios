da7c0be8c3e41ce27dfe0d85adc38e7eddd42dcb
carina-jg <carinagroisman@gmail.com>
2022-11-13 16:43:50 -0300

    ultimas modificaciones

--- api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
+++ api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
@@ -2,12 +2,12 @@ package com.dh.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @EnableFeignClients
+@EnableDiscoveryClient
 public class ApiCatalogApplication {
 
     public static void main(String[] args) {
