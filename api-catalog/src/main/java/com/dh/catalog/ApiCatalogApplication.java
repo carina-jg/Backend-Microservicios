134b10dc597d5fd950ad0b2d83f8a4c736917b12
carina-jg <carinagroisman@gmail.com>
2022-11-12 22:40:57 -0300

    api catalog

--- api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
+++ api-catalog/src/main/java/com/dh/catalog/ApiCatalogApplication.java
@@ -3,8 +3,10 @@ package com.dh.catalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 @EnableFeignClients
 public class ApiCatalogApplication {
 
