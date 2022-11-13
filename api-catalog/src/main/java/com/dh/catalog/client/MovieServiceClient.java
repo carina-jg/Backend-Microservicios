aca002ed0eebf86e1dcbe89b85d9d1129cd19a47
carina-jg <carinagroisman@gmail.com>
2022-11-13 17:37:28 -0300

    final

--- api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
+++ api-catalog/src/main/java/com/dh/catalog/client/MovieServiceClient.java
@@ -1,7 +1,9 @@
 package com.dh.catalog.client;
 
+import com.dh.catalog.configuration.LoadBalancerConfiguration;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @FeignClient(name="api-movie")
+@LoadBalancerClient(name = "api-movie", configuration = LoadBalancerConfiguration.class)
 public interface MovieServiceClient {
 
 	@GetMapping("/api/v1/movies/{genre}")
